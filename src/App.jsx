32389f70664e54b09480d83e6edabc63944b08f9
skaas <skaparty@gmail.com>
2025-07-09 22:05:36 +0900

    초기 세팅 완료

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
+import React from 'react';
 import RecommendForm from './components/RecommendForm';
 
 function App() {
+  console.log('App 컴포넌트 렌더링됨');
   return (
     <div className="bg-gray-100 min-h-screen flex items-center justify-center p-4">
+      <div className="absolute top-2 left-2 text-xs text-gray-400">[DEBUG] App.jsx 렌더링됨</div>
       <RecommendForm />
     </div>
   );
